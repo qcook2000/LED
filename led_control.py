2fd5a83adf8a8b36849c25b146b3ac5edc358382
Quenton Cook <quenton@remind.com>
2017-08-03 02:09:03 -0700

    RAINBOW

--- led_control.py
+++ led_control.py
@@ -1,24 +1,36 @@
 import bibliopixel
+from bibliopixel import *
+from bibliopixel.animation import *
+from bibliopixel.layout import *
+from bibliopixel.drivers.SimPixel import *
+
 # causes frame timing information to be output
 bibliopixel.log.setLogLevel(bibliopixel.log.DEBUG)
 
-# Load driver for the AllPixel
-from bibliopixel.drivers.SimPixel import *
 # set number of pixels & LED type here http://simpixel.io/
 driver = SimPixel(num = 300)
 
 # load the LEDStrip class
-from bibliopixel.layout import *
-led = Strip(driver)
+ledStrip = Strip(driver)
 
-# load channel test animation
-from bibliopixel.animation import StripChannelTest
-anim = StripChannelTest(led)
+
+class BasicAnimTest(BaseStripAnim):
+    def __init__(self, led):
+        super(BasicAnimTest, self).__init__(led)
+        #do any initialization here
+  
+    def step(self, amt = 1):
+        speed = 15
+        for i in range(300):
+            self._led.set(i, colors.hue2rgb((i*4+(self._step*speed))%256))
+        self._step += amt
+
+
 
 try:
-    # run the animation
-    anim.run()
+    anim = BasicAnimTest(ledStrip)
+    anim.run(fps=45)
 except KeyboardInterrupt:
-    # Ctrl+C will exit the animation and turn the LEDs offs
+    #turn everything off if Ctrl+C is pressed
     led.all_off()
     led.update()
