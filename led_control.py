9e6e13dd8af383f585080261505a644e0ef878ab
Quenton Cook <quenton@remind.com>
2017-08-03 20:13:03 -0700

    Use library

--- led_control.py
+++ led_control.py
@@ -4,6 +4,8 @@ from bibliopixel.animation import *
 from bibliopixel.layout import *
 from bibliopixel.drivers.SimPixel import *
 
+from BiblioAnimations.BiblioPixelAnimations.strip import Rainbows
+
 # causes frame timing information to be output
 # bibliopixel.log.setLogLevel(bibliopixel.log.DEBUG)
 
@@ -13,23 +15,9 @@ driver = SimPixel(num = 300)
 # load the LEDStrip class
 ledStrip = Strip(driver)
 
-
-class BasicAnimTest(BaseStripAnim):
-    def __init__(self, led):
-        super(BasicAnimTest, self).__init__(led)
-        #do any initialization here
-  
-    def step(self, amt = 1):
-        speed = 15
-        for i in range(300):
-            self._led.set(i, colors.hue2rgb((i*4+(self._step*speed))%256))
-        self._step += amt
-
-
-
 try:
-    anim = BasicAnimTest(ledStrip)
-    anim.run(fps=45)
+    anim = Rainbows.RainbowCycle(ledStrip)
+    anim.run()
 except KeyboardInterrupt:
     #turn everything off if Ctrl+C is pressed
     led.all_off()
