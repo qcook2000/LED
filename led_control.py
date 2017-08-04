9360f776ac7b3416c03a3357ada8c896096b2006
Quenton Cook <quenton@remind.com>
2017-08-03 22:05:46 -0700

    Add animations

--- led_control.py
+++ led_control.py
@@ -4,16 +4,41 @@ from bibliopixel.animation import *
 from bibliopixel.layout import *
 from bibliopixel.drivers.SimPixel import *
 
-from BiblioAnimations.BiblioPixelAnimations.strip import Rainbows
+from BiblioAnimations.BiblioPixelAnimations.strip import *
 
 # causes frame timing information to be output
-# bibliopixel.log.setLogLevel(bibliopixel.log.DEBUG)
+bibliopixel.log.setLogLevel(bibliopixel.log.DEBUG)
 
 # set number of pixels & LED type here http://simpixel.io/
 driver = SimPixel(num = 300)
 
 # load the LEDStrip class
 ledStrip = Strip(driver)
+
+animations = [
+        Alternates.Alternates(),
+        BinaryEpochClock.BEClock(),
+        ColorChase.ColorChase(),
+        ColorFade.ColorFade(),
+        ColorPattern.ColorPattern(),
+        ColorWipe.ColorWipe(),
+        FireFlies.FireFlies(),
+        HalvesRainbow.HalvesRainbow(),
+        LarsonScanners.LarsonRainbow(),
+        LarsonScanners.LarsonScanner(),
+        LinearRainbow.LinearRainbow(),
+        PartyMode.PartyMode(),
+        PixelPingPong.PixelPingPong(),
+        RGBClock.RGBClock(),
+        Rainbows.Rainbow(),
+        Rainbows.RainbowCycle(),
+        Searchlights.Searchlights(),
+        Wave.Wave(),
+        Wave.WaveMove(),
+        WhiteTwinkle.WhiteTwinkle(),
+        hexclock.HEXClock() ]
+
+animationIndex = 0
 
 try:
     anim = Rainbows.RainbowCycle(ledStrip)
